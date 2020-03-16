804f3db3554c24faa5d6483e58e9136041b307d1
flmngwllm <flmngwllm232@gmail.com>
2020-03-16 18:26:16 -0400

    corrected slector mistake for the dice btn in javascript

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ var roundScore = 0
 
 var activePlayer = 0
 
-// document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 // var x = document.querySelector('#score-0')
 
-document.querySelector('.dice').style.css.display = 'none'
+document.querySelector('.dice').style.display = 'none'
 
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
@@ -21,11 +20,18 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 
     //2. Display the result
     var diceDOM = document.querySelector('.dice')
-    diceDOM.style.css.display = 'block';
+    diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png'
     
 
     //3. Update the round score If the rolled number was NOT a 1
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+    } else {
+
+    }
 });
 
 
