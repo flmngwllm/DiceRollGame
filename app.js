d6c4f75d3f4ac251be935866c3b69ce9827cab62
flmngwllm <flmngwllm232@gmail.com>
2020-03-16 16:05:33 -0400

    added variable to display the result of the dice rolls and set the scores to zero

--- app.js
+++ app.js
@@ -2,19 +2,30 @@ var scores = [0,0]
 var roundScore = 0
 
 var activePlayer = 0
-var dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
+// var x = document.querySelector('#score-0')
 
+document.querySelector('.dice').style.css.display = 'none'
 
-var x = document.querySelector('#score-0')
-
-// document.querySelector('.dice').style.css.display = 'none'
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
     //1. Need a random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.css.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png'
+    
+
+    //3. Update the round score If the rolled number was NOT a 1
 });
 
 
