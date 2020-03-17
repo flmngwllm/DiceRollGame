0214bbe04736b12ccfbaf032305a79bb0d0e49b7
flmngwllm <flmngwllm232@gmail.com>
2020-03-16 21:43:21 -0400

    created a state to check whether game is currently being played or not

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 var scores, roundScore, activePlayer 
 init();
+var gamePlaying = true 
 
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
 // var x = document.querySelector('#score-0')
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-    //1. Need a random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+    if(gamePlaying){
+//1. Need a random number
+var dice = Math.floor(Math.random() * 6) + 1;
 
-    //2. Display the result
-    var diceDOM = document.querySelector('.dice')
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png'
+//2. Display the result
+var diceDOM = document.querySelector('.dice')
+diceDOM.style.display = 'block';
+diceDOM.src = 'dice-' + dice + '.png'
+
+
+//3. Update the round score If the rolled number was NOT a 1
+if (dice !== 1) {
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+
+} else {
+    nextPlayer();
     
-
-    //3. Update the round score If the rolled number was NOT a 1
-    if (dice !== 1) {
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-
-    } else {
-        nextPlayer();
-        
+}
     }
+    
+    
 });
 
 
 
 document.querySelector('.btn-hold').addEventListener('click', function(){
-     // Add current score to global score
+    if(gamePlaying){
+         // Add current score to global score
      scores[activePlayer] += roundScore
      
      // update the UI
@@ -40,10 +46,12 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
         document.querySelector('dice').style.display = 'none'
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
-
+        gamePlaying = false;
     } else {
         nextPlayer();
     }
+    } 
+   
 
      
 })
